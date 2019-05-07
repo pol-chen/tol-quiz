5a0e791688a706f0f0fee206452c52dd0c05b172
PolarisChen <polaris_chen@163.com>
2019-05-07 18:32:28 -0400

    Show questions

--- js/main.js
+++ js/main.js
@@ -20,28 +20,73 @@ function readJson(path, next) {
 
 var current = 0;
 
+function pickFeedback(qid) {
+  var feedback = '';
+  for (var option of optionList[qid].correct) {
+    if (!option.isUsedAsOption && !option.isUsedAsFeedback) {
+      option.isUsedAsFeedback = true;
+      feedback = option.text;
+      break;
+    }
+  }
+  return feedback;
+}
+
+function pickOptions(qid) {
+  var options = [];
+  
+  // Pick correct option as 0
+  for (var option of optionList[qid].correct) {
+    if (!option.isUsedAsOption && !option.isUsedAsFeedback) {
+      option.isUsedAsOption = true;
+      options[0] = option;
+      break;
+    }
+  }
+
+  // Pick incorrect options as 1, 2, 3
+  var count = 0;
+  for (var option of optionList[qid].incorrect) {
+    if (!option.isUsedAsOption && !option.isUsedAsFeedback) {
+      option.isUsedAsOption = true;
+      options[++count] = option;
+      if (count === 3) break;
+    }
+  }
+
+  // Shuffle options
+  options.sort(function() {
+    return Math.random() - 0.5;
+  });
+  console.log('OPTIONS', options);
+
+  return options;
+}
+
 function loadQuestion() {
   console.log('LOAD Q' + (current + 1));
   var quiz = questionList;
-  var p = quiz[current++];
-  console.log(quiz);
+  var q = quiz[current++];
+  q.options = pickOptions(q.qid);
+  q.feedback = pickFeedback(q.qid);
+  // console.log(quiz);
   $('#scene-question h2').text('Question ' + current);
-  $('#scene-question p').text(p.text);
+  $('#scene-question p').text(q.text);
   $('#scene-question .btn-continue').addClass('btn-disabled');
   $('#scene-question .select').empty();
-  p.options.forEach(function(option, i) {
-    // console.log(option, i);
+  q.options.forEach(function(option, i) {
+    console.log(option, i);
     $('#scene-question .select').append(buildOption(option, i));
-    if (option.correct) {
-      $('#scene-correct p').text(option.feedback);
+    if (option.isCorrect) {
+      $('#scene-correct p').text(q.feedback);
     } else {
-      $('#scene-wrong p').text(option.feedback);
+      $('#scene-wrong p').text(q.feedback);
     }
   });
 }
 
 function buildOption(op, i) {
-  var feedback = op.correct ? 'correct' : 'wrong';
+  var feedback = op.isCorrect ? 'correct' : 'wrong';
   var nums = ['A', 'B', 'C', 'D'];
   var num = nums[i];
   return '<div class="frame option" data-next="#scene-' + feedback + '">\
@@ -96,7 +141,7 @@ function registerEvents() {
     }
   })
   $('.btn-next-question').click(function () {
-    if (current == quiz.length) {
+    if (current == questionList.length) {
       showScene('#scene-end');
     } else {
       showScene('#scene-question', loadQuestion);
