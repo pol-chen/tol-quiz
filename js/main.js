26c7e1d3678d23b8c4cffa26c78f2047a67acd71
PolarisChen <polaris_chen@163.com>
2019-05-07 10:27:12 -0400

    Calculate IRT correctness

--- js/main.js
+++ js/main.js
@@ -1,61 +1,62 @@
 
 /* Quiz */
+
 var questions = [{
-    desc: 'When light from an object reaches our eyes, which part(s) of the eye refracts the light?',
+    text: 'When light from an object reaches our eyes, which part(s) of the eye refracts the light?',
     image: '',
     options: [
       {
-        desc: 'Cornea and Lens',
+        text: 'Cornea and Lens',
         correct: true,
         feedback: 'Correct! When light from an object reaches our eyes, the cornea and lens refracts the light.'
       },
       {
-        desc: 'Retina',
+        text: 'Retina',
         correct: false,
         feedback: 'Ooops! Retina does not refract light, it is where light gets focused in a person who has perfect vision.'
       },
       {
-        desc: 'Glass',
+        text: 'Glass',
         correct: false,
         feedback: 'Ooops! Light enters the eye through the pupil, but pupil does not refracts light.'
       }
     ]
   }, {
-    desc: 'In order to have perfect vision, where does light need to be focused in the eye?',
+    text: 'In order to have perfect vision, where does light need to be focused in the eye?',
     image: '',
     options: [
       {
-        desc: 'In between lens and retina',
+        text: 'In between lens and retina',
         correct: false,
         feedback: 'Ooops! When light gets focused before the retina, the person will have a blurry vision.'
       },
       {
-        desc: 'Cornea and Lens',
+        text: 'Cornea and Lens',
         correct: false,
         feedback: 'Ooops! Cornea and lens are where light gets refracted, not where light gets focused in order to have perfect vision.'
       },
       {
-        desc: 'Retina',
+        text: 'Retina',
         correct: true,
         feedback: 'Good job! In a person who has perfect vision, the refracted light is actually focused on the retina.'
       }
     ]
   }, {
-    desc: 'When light gets focused before the retina, what will it cause?',
+    text: 'When light gets focused before the retina, what will it cause?',
     image: '',
     options: [
       {
-        desc: 'Hyperopia and hypermetropia',
+        text: 'Hyperopia and hypermetropia',
         correct: false,
         feedback: 'Ooops! When light gets focused before the retina, the person will have a blurry vision.'
       },
       {
-        desc: 'Myopia',
+        text: 'Myopia',
         correct: true,
         feedback: 'Good job! Myopia is also called nearsightedness, which causes blurry vision when looking at distant objects.'
       },
       {
-        desc: 'Farsightedness',
+        text: 'Farsightedness',
         correct: false,
         feedback: 'Ooops! Farsightedness is also called hyperopia or hypermetropia, which is the defect when light gets focused behind the retina.'
       }
@@ -70,7 +71,7 @@ function loadQuestion() {
 
   var p = quiz[current++];
   $('#scene-question h2').text('Question ' + current);
-  $('#scene-question p').text(p.desc);
+  $('#scene-question p').text(p.text);
   if (p.image.length > 0) {
     $('#scene-question p').append('<img src="images/' + p.image + '" alt="">');
   }
@@ -92,7 +93,7 @@ function buildOption(op, i) {
   var nums = ['A', 'B', 'C', 'D'];
   var num = nums[i];
   return '<div class="frame option" data-next="#scene-' + feedback + '">\
-    <p>' + num + '. ' + op.desc + '</p>\
+    <p>' + num + '. ' + op.text + '</p>\
   </div>';
 }
 
@@ -210,7 +211,7 @@ function readQuestions(data) {
     var q = data[i];
     var question = {
       qid: q.Question_id,
-      desc: q.Question_text,
+      text: q.Question_text,
       options: []
     }
     questionList.push(question);
@@ -221,6 +222,7 @@ function readQuestions(data) {
 function readOptions(data) {
   // Read data
   var optionList = [];
+  var difficultyList = [];
   var quizScoreList = [];
   var avgScoreList = [];
   for (var i = 0; i < data.length; i++) {
@@ -228,12 +230,13 @@ function readOptions(data) {
     var qid = o.Question_id;
     if (optionList[qid] == null) {
       optionList[qid] = [];
+      difficultyList[qid] = 0;
       quizScoreList[qid] = 0;
       avgScoreList[qid] = 0;
     }
     var option = {
       qid: qid,
-      desc: o.Answer_text,
+      text: o.Answer_text,
       keywords: [], // Default, analyze later in analyzeKeywords
       isCorrect: false, // Default, analyze later in analyzeOption
       isUseful: false, // Default, analyze later in analyzeOption
@@ -241,42 +244,50 @@ function readOptions(data) {
       isUsedAsFeedback: false,
       score: Number(o.Student_score_on_question),
       quizScore: Number(o.Quiz_score),
-      avgScore: Number(o.Average_quizzes_score)
+      avgScore: Number(o.Average_quizzes_score),
+      irtCorrectness: 0.0 // Default, calculate later in calculateIRTCorrectness
     }
     optionList[qid].push(option);
+    difficultyList[qid] += option.score;
     quizScoreList[qid] += option.quizScore;
     avgScoreList[qid] += option.avgScore;
   }
 
-  // Calculate scores
-  for (var i = 0; i < optionList.length; i++) {
-    var optionCount = optionList[qid].length;
+  // Calculate difficulty, quiz and avg scores
+  for (var i = 1; i < optionList.length; i++) {
+    var optionCount = optionList[i].length;
+
+    var score = difficultyList[i];
+    difficultyList[i] = score / optionCount;
+
     var quizScore = quizScoreList[i];
-    if (quizScore != null) {
-      quizScoreList[i] = quizScore / optionCount;
-    }
+    quizScoreList[i] = quizScore / optionCount;
+
     var avgScore = avgScoreList[i];
-    if (avgScore != null) {
-      avgScoreList[i] = avgScore / optionCount;
-    }
+    avgScoreList[i] = avgScore / optionCount;
   }
   console.log('readOptions', optionList);
+  console.log('difficultyList', difficultyList);
   console.log('quizScoreList', quizScoreList);
   console.log('avgScoreList', avgScoreList);
 
   // Analyze keywords
-  for (var i = 0; i < optionList.length; i++) {
-    var options = optionList[i];
-    for (var op in options) {
-      op.keywords = analyzeKeywords(op.desc);
-    }
-  }
+  // for (var i = 0; i < optionList.length; i++) {
+  //   var options = optionList[i];
+  //   for (var op in options) {
+  //     op.keywords = analyzeKeywords(op.text);
+  //   }
+  // }
 
   // Analyze options
-  for (var i = 0; i < optionList.length; i++) {
+  for (var i = 1; i < optionList.length; i++) {
     var options = optionList[i];
-    for (var op in options) {
+    for (var j = 0; j < options.length; j++) {
+      var op = options[j];
+      console.log('[BEFORE]', op.irtCorrectness);
       analyzeOption(op, quizScoreList[i], avgScoreList[i]);
+      calculateIRTCorrectness(op, difficultyList[i]);
+      console.log('[AFTER]', op.irtCorrectness);
     }
   }
 }
@@ -285,7 +296,7 @@ function analyzeKeywords(text) {
   return [];
 }
 
-// analyzeCorrectness(desc, o.Student_score_on_question, o.Quiz_score, o.Average_quizzes_score)
+// analyzeCorrectness(text, o.Student_score_on_question, o.Quiz_score, o.Average_quizzes_score)
 function analyzeOption(option, quizScoreAvg, avgScoreAvg) {
   // Check score
   if (option.score <= 0.5) {
@@ -300,11 +311,17 @@ function analyzeOption(option, quizScoreAvg, avgScoreAvg) {
     return;
   }
 
-  // Check correct
+  // Check quiz and avg score
   if (option.quizScore >= quizScoreAvg || option.avgScore >= avgScoreAvg) {
     option.isCorrect = true;
     option.isUseful = true;
   }
+}
+
+function calculateIRTCorrectness(option, difficulty) {
+  // Use IRT Rasch Model
+  var ability = (option.quizScore + option.avgScore) / 2.0;
+  option.irtCorrectness = Math.exp(ability - difficulty) / (1 + Math.exp(ability - difficulty));
 }
 
 $(document).ready(function () {
